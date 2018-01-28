a27df273a062f1c8aa6ad9a56b28bd7fc827049c
serhii.harbovskyi <garbovsky.s@gmail.com>
2018-01-28 16:14:07 +0200

    DZ 4 start work

---
+++ DZ4_Harbovskyi_Serhii/DZ4_Harbovskyi_Serhii/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DZ4_Harbovskyi_Serhii
+//
+//  Created by Harbovskyi Serhii on 1/28/18.
+//  Copyright © 2018 Harbovskyi Serhii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
